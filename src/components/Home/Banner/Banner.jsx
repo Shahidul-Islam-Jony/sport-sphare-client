f2d17ffae1740d27b6dd7560d1d4f9bf496ff5a1
Shahidul-Islam-Jony <shahidulislam13sf@gmail.com>
2024-01-01 01:32:01 +0600

    Update some code of Banner components and it's done

--- src/components/Home/Banner/Banner.jsx
+++ src/components/Home/Banner/Banner.jsx
@@ -1,6 +1,16 @@
 
 import banner1 from '../../../assets/images/banner1.jpg'
 import banner2 from '../../../assets/images/banner2.jpg'
+import banner3 from '../../../assets/images/banner3.jpg'
+import banner4 from '../../../assets/images/banner4.jpg'
+import banner5 from '../../../assets/images/banner5.jpg'
+import banner6 from '../../../assets/images/banner6.jpg'
+import banner7 from '../../../assets/images/banner7.jpg'
+import banner8 from '../../../assets/images/banner8.jpg'
+import banner9 from '../../../assets/images/banner9.jpg'
+import banner10 from '../../../assets/images/banner10.jpg'
+import banner11 from '../../../assets/images/banner11.jpg'
+import banner13 from '../../../assets/images/banner13.png'
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
@@ -15,7 +25,7 @@ const Banner = () => {
     return (
         <div className='mt-5'>
             <Swiper
-                loop={true}
+                // loop={true}
                 pagination={{ clickable: true }}
                 modules={[Pagination,EffectCoverflow,Autoplay]}
                 autoplay={{ delay: 5000 }}
@@ -25,31 +35,50 @@ const Banner = () => {
                 slidesPerView={'auto'}
                 coverflowEffect={{
                     rotate: 50,
-                    stretch: 0,
-                    depth: 100,
-                    modifier: 1,
+                    depth: 2000,
                     slideShadows: true,
                 }}
                 className="mySwiper"
             >
                 <SwiperSlide>
-                    <img src={banner1} className='h-[500px] w-11/12 rounded-lg' />
+                    <img src={banner11} className='h-[500px] w-11/12 bg-cover md:w-2/3 rounded-lg' />
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img src={banner2} className='h-[500px] w-11/12 rounded-lg' />
+                    <img src={banner1} className='h-[500px] w-11/12 bg-cover md:w-2/3 rounded-lg' />
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img src={banner1} className='h-[500px] w-11/12 rounded-lg' />
+                    <img src={banner13} className='h-[500px] w-11/12 bg-cover md:w-2/3 rounded-lg' />
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img src={banner2} className='h-[500px] w-11/12 rounded-lg' />
+                    <img src={banner3} className='h-[500px] w-11/12 bg-cover md:w-2/3 rounded-lg' />
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img src={banner1} className='h-[500px] w-11/12 rounded-lg' />
+                    <img src={banner4} className='h-[500px] w-11/12 bg-cover md:w-2/3 rounded-lg' />
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img src={banner2} className='h-[500px] w-11/12 rounded-lg' />
+                    <img src={banner6} className='h-[500px] w-11/12 bg-cover md:w-2/3 rounded-lg' />
                 </SwiperSlide>
+                <SwiperSlide>
+                    <img src={banner8} className='h-[500px] w-11/12 bg-cover md:w-2/3 rounded-lg' />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={banner9} className='h-[500px] w-11/12 bg-cover md:w-2/3 rounded-lg' />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={banner10} className='h-[500px] w-11/12 bg-cover md:w-2/3 rounded-lg' />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={banner7} className='h-[500px] w-11/12 bg-cover md:w-2/3 rounded-lg' />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={banner2} className='h-[500px] w-11/12 bg-cover md:w-2/3 rounded-lg' />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={banner5} className='h-[500px] w-11/12 bg-cover md:w-2/3 rounded-lg' />
+                </SwiperSlide>
+                
+               
+               
 
             </Swiper>
         </div>
